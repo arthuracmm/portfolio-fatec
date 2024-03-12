3312547abb1c363d4b63d41bd9495a2aa4683dae
Arthur Cesar <130599250+arthuracmm@users.noreply.github.com>
2024-03-12 00:49:13 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function imgHover() {
+    projectImage.src = './assets/img/will.png';
+}
+function imgHover2() {
+    projectImage.src = './assets/img/will2.png';
+}
+
+projectImage.addEventListener('mouseover', imgHover); 
+projectImage.addEventListener('mouseleave', imgHover2); 
